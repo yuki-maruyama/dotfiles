84b43bc8a9bf2a5cfd58aa23a1efdfcb77c72bcf
Yuki Maruyama <dev@32ba.net>
2024-05-09 16:32:29 +0900

    Update: Symlink

--- init.sh
+++ init.sh
@@ -4,3 +4,24 @@ if [ "$(uname)" == "Darwin" ] ; then
 	.bin/macos.sh
 fi
 
+# symlink
+IGNORE_PATTERN="^\.(git|bin|config)"
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+
+DIR_SYMLINKS=(
+	".config/git/ignore"
+)
+
+## home root dir symlink
+for f in .??*; do
+    [[ $f =~ $IGNORE_PATTERN ]] && continue
+		[ "$f" = ".DS_Store" ] && continue
+
+    ln -snfv ${PWD}/"$f" ~
+done
+
+## not root dir symlink
+for f in ${DIR_SYMLINKS[@]}; do
+	ln -snfv ${PWD}/"$f" ~/"$f"
+done
