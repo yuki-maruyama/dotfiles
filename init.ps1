41daa9387ed4efbedfecdd23221dc29ba86ded2f
Yuki Maruyama <dev@32ba.net>
2024-09-26 17:40:02 +0900

    add windows setup

---
+++ init.ps1
@@ -0,0 +1,40 @@
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptDirectory = Split-Path -Parent $scriptPath
+$profileDirectory = Split-Path -Parent $PROFILE
+
+# set execution policy to Administrators
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+# Winget package list
+$wingetPackageList = @(
+    "7zip.7zip",
+    "Starship.Starship",
+    "Git.Git"
+)
+
+Write-Host "Installing packages from winget"
+# install
+foreach ($wingetPackage in $wingetPackageList) {
+  Write-Host "Installing $wingetPackage"
+  winget install -e --id $wingetPackage 
+  Write-Host `n
+}
+
+# symlink 
+$symlinks = @(
+    ("$HOME\.config\starship.toml", "$scriptDirectory\.config\starship.toml"),
+    ("$HOME\.config\git\ignore", "$scriptDirectory\.config\git\ignore"),
+    ("$PROFILE", "$scriptDirectory\Microsoft.PowerShell_profile.ps1")
+)
+
+Write-Host "Creating symlink"
+foreach ($symlink in $symlinks) {
+    $target = $symlink[0]
+    $link = $symlink[1]
+    Write-Host "Creating symlink $link -> $target"
+    New-Item -ItemType SymbolicLink -Path $target -Target $link -Force
+}
+
+# wait for user input
+Write-Host "Complete. Press any key to continue..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
